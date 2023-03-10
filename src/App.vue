7dfa4081746283fb46d9fa783ba8d19b4c82d569
grdhog <grdhog@users.noreply.github.com>
2023-03-11 10:37:36 +1300

    2 files modified

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,19 @@
 <template>
   <div id="app">
-    <DataTable></DataTable>
+    <DataTable collection="key"></DataTable>
   </div>
 </template>
 
 <script>
 import DataTable from './components/DataTable.vue';
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
-import { collection, getDocs, addDoc } from 'firebase/firestore';
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCDpkfdEY7E-5x3cYKuqBEsw53DYUoz5MI",
-  authDomain: "project1-5e2f9.firebaseapp.com",
-  projectId: "project1-5e2f9",
-  storageBucket: "project1-5e2f9.appspot.com",
-  messagingSenderId: "509927420951",
-  appId: "1:509927420951:web:e32cfbaa1d2e9c324a1852"
-};
 
 export default {
   name: 'App',
   components: {
     DataTable,
   },
-  async mounted() {
-    console.log('I just mounted bro!');
-    const app = initializeApp(firebaseConfig);
-    const db = getFirestore(app);
-    const querySnapshot = await getDocs(collection(db, 'keys'));
-    querySnapshot.forEach((doc) => {
-      console.log(`${doc.id} => ${doc.data().KeyName}`);
-    });
+  mounted() {
+    console.log('App mounted');
   },
 };
 </script>
--- src/components/DataTable.vue
+++ src/components/DataTable.vue
@@ -31,9 +31,47 @@
   </table>
 </template>
 
-
 <script>
+
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { collection, getDocs, addDoc } from 'firebase/firestore';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCDpkfdEY7E-5x3cYKuqBEsw53DYUoz5MI",
+  authDomain: "project1-5e2f9.firebaseapp.com",
+  projectId: "project1-5e2f9",
+  storageBucket: "project1-5e2f9.appspot.com",
+  messagingSenderId: "509927420951",
+  appId: "1:509927420951:web:e32cfbaa1d2e9c324a1852"
+};
+
 export default {
-  name: 'DataTable'
+  name: 'DataTable',
+  props: ['collection'],
+  mounted() {
+    this.getData();
+  },
+  methods: {
+    getData() {
+      console.log('getData called!');
+      const app = initializeApp(firebaseConfig);
+      const db = getFirestore(app);
+      getDocs(collection(db, 'keys'))
+        .then(this.handleSuccess, this.handleFailure);
+    },
+    handleSuccess(value){
+      console.log(value);
+      /*
+      querySnapshot.forEach((doc) => {
+        console.log(`${doc.id} => ${doc.data().KeyName}`);
+      });
+      */
+    },
+    handleFailure(value){
+      console.log(value);
+    }
+  }
 };
 </script>
